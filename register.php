afc628b2a1a6dd1d154ee5288c71904b6d9228fa
jaymalave <jaym.github@gmail.com>
2022-04-22 16:12:40 +0530

    feat: add input clean

--- register.php
+++ register.php
@@ -1,3 +1,47 @@
+<?php 
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+	//Login button was pressed
+	
+}
+
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	
+	$firstName = sanitizeFormString($_POST['firstName']);
+
+	$lastName = sanitizeFormString($_POST['lastName']);
+
+	$email = sanitizeFormString($_POST['email']);
+
+	$email2 = sanitizeFormString($_POST['email2']);
+
+	$password = sanitizeFormPassword($_POST['password']);
+
+	$password2 = sanitizeFormPassword($_POST['password2']);
+
+}
+?>
+
 <html>
 <head>
 	<title>Spotify But Better</title>
@@ -49,7 +93,6 @@
 				<label for="password2">Confirm Password</label>
 				<input id="password2" name="password2" type="password" required>
 			</p>
-			
 
 			<button type="submit" name="registerButton">Sign Up</button>
 			
