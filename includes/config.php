1256568a8f6341032bddb7b47dbc2eddda0ce6ef
jaymalave <jaym.github@gmail.com>
2022-04-23 17:41:55 +0530

    feat: add user session

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,8 @@
 <?php
   ob_start();
+  session_start();
+
+
   $timezone = date_default_timezone_set("Asia/Calcutta");
 
   $con = mysqli_connect("localhost", "root", "", "better-spotify");
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+include("includes/config.php");
+
+  if(isset($_SESSION['userLoggedIn'])){
+	  $userLoggedIn = $_SESSION['userLoggedIn'];
+  }
+  else {
+	  header("Location: register.php");
+  }
+?>
+
+
 <html>
 <head>
 	<title>Spotify But Better</title>
