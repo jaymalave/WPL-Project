b36e339be640c8b2c45e04c19d201b9d08d1cd84
jaymalave <jaym.github@gmail.com>
2022-05-01 20:50:35 +0530

    feat: add script for ajax push

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,37 @@ var shuffle = false;
 var userLoggedIn;
 var timer;
 
+
+$(document).click(function(click) {
+	var target = $(click.target);
+
+	if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
+		hideOptionsMenu();
+	}
+});
+
+$(window).scroll(function() {
+	hideOptionsMenu();
+});
+
+$(document).on("change", "select.playlist", function() {
+	var select = $(this);
+	var playlistId = select.val();
+	var songId = select.prev(".songId").val();
+
+	$.post("includes/handlers/ajax/addToPlaylist.php", { playlistId: playlistId, songId: songId})
+	.done(function(error) {
+
+		if(error != "") {
+			alert(error);
+			return;
+		}
+
+		hideOptionsMenu();
+		select.val("");
+	});
+});
+
 function openPage(url) {
 
 	if(timer != null) {
@@ -73,6 +104,30 @@ function deletePlaylist(playlistId) {
 
 
 	}
+}
+
+
+function hideOptionsMenu() {
+	var menu = $(".optionsMenu");
+	if(menu.css("display") != "none") {
+		menu.css("display", "none");
+	}
+}
+
+function showOptionsMenu(button) {
+	var songId = $(button).prevAll(".songId").val();
+	var menu = $(".optionsMenu");
+	var menuWidth = menu.width();
+	menu.find(".songId").val(songId);
+
+	var scrollTop = $(window).scrollTop(); //Distance from top of window to top of document
+	var elementOffset = $(button).offset().top; //Distance from top of document
+
+	var top = elementOffset - scrollTop;
+	var left = $(button).position().left;
+
+	menu.css({ "top": top + "px", "left": left - menuWidth + "px", "display": "inline" });
+
 }
 
 
