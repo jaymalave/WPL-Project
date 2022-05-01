c4298272b28955c2c926407063ff97b716de1473
jaymalave <jaym.github@gmail.com>
2022-05-01 18:57:23 +0530

    fix: minor fixes

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -32,6 +32,7 @@ if(isset($_POST['registerButton'])) {
 
     if($wasSuccessful == true) {
         $_SESSION['userLoggedIn'] = $username;
+
 		header("Location: index.php");
 	}
 
--- includes/navbarContainer.php
+++ includes/navbarContainer.php
@@ -26,7 +26,7 @@
 	</div>
 
 	<div class="navItem">
-		<a href="settings.php" class="navItemLink"><?php echo $_SESSION['full_name'] ?></a>
+		<a href="settings.php" class="navItemLink"><?php echo $_SESSION['userLoggedIn'] ?></a>
 	</div>
 </div>
 
