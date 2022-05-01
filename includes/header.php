bea77dc53112f2551b334417e4130d0935bee2ec
jaymalave <jaym.github@gmail.com>
2022-05-01 11:30:19 +0530

    feat: add search and browse pages

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@ include("includes/config.php");
 
 if(isset($_SESSION['userLoggedIn'])) {
 	$userLoggedIn = $_SESSION['userLoggedIn'];
+	echo "<script>userLoggedIn = '$userLoggedIn';</script>";
+
 }
 else {
 	header("Location: register.php");
