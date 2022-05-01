0ab7ca655839b8651e46b5117e3288852ebbec67
jaymalave <jaym.github@gmail.com>
2022-05-01 12:06:03 +0530

    feat: add my music page, user and pl classes

--- browse.php
+++ browse.php
@@ -2,7 +2,7 @@
 include("includes/includedFiles.php"); 
 ?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
+<h1 class="pageHeadingBig">Welcome to Spotify, but better</h1>
 
 <div class="gridViewContainer">
 
@@ -10,10 +10,6 @@ include("includes/includedFiles.php");
 		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
 		while($row = mysqli_fetch_array($albumQuery)) {
-			
-
-
-
 			echo "<div class='gridViewItem'>
 					<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
 						<img src='" . $row['artworkPath'] . "'>
@@ -22,10 +18,7 @@ include("includes/includedFiles.php");
 							. $row['title'] .
 						"</div>
 					</span>
-
 				</div>";
-
-
 
 		}
 	?>
